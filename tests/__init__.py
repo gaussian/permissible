9f9ad337e324f5a00e89bd4c21b3fa879aa9946f
Kut Akdogan <kut.akdogan@gmail.com>
2020-09-11 11:09:55 -0400

    Add (empty) permissible tests

---
+++ tests/__init__.py
@@ -0,0 +1,3 @@
+from neutron.tests.setup import setup_test_app
+
+setup_test_app(__package__)
---
+++ tests/tests.py
@@ -0,0 +1,8 @@
+"""
+Neutron (a Visor module)
+Author: Kut Akdogan
+(c) 2016- Gaussian Holdings, LLC.
+
+This codebase is confidential and proprietary.
+No license for use, viewing, or reproduction without explicit written permission.
+"""
