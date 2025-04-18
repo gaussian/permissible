d484bd3c442469c2acfd068baaeacd1f3374ba2f
Kut Akdogan <kut.akdogan@gmail.com>
2025-04-18 09:57:09 -0400

    Remove NoPermissionsIfListMixin

--- permissible/views.py
+++ permissible/views.py
@@ -4,20 +4,8 @@ Author: Kut Akdogan & Gaussian Holdings, LLC. (2016-)
 """
 
 from django.conf import settings
-from rest_framework.permissions import IsAuthenticated
 
 from permissible.models.permissible_mixin import PermissibleMixin
-
-
-class NoPermissionsIfListMixin:
-    def get_permissions(self):
-        # For certain actions, don't bother checking for permissions, as
-        # we filter down the objects that are returned (listing/searching),
-        # or check permissions some other way
-        list_actions = getattr(self, "LIST_ACTIONS", ("list",))
-        if self.action in list_actions:
-            return [IsAuthenticated()]
-        return super().get_permissions()
 
 
 class CheckViewConfigMixin:
